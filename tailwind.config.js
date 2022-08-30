7396606daa474eb1a5b28e5c2b72700bcb16b57b
Adam N <adaamangelow@gmail.com>
2022-08-30 16:48:18 -0300

    wip: login

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1200px'
+      // => @media (min-width: 1536px) { ... }
+    },
     colors: {
       text: '#FFFFFF',
       boxcolor: '#434343',
