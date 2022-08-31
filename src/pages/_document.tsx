7f6833d5b3593d79a90fa1777ac2a557e0aecbcc
Adam N <adaamangelow@gmail.com>
2022-08-30 21:18:27 -0300

    fix: import animate style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet" />
             <link rel="shortcut icon" href="/crystal.png" type="image/x-icon" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
         </Head>
         <body>
           <Main />
