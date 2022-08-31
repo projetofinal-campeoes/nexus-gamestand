53a90b9c3bd3cbe2f6727d8b99e7ebbb339267f8
=Welton Santos <weltonsantosfr@gmail.com>
2022-08-31 18:42:27 -0300

    feat: conclude profile modal

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -16,9 +16,9 @@ const Profile = ({ checked, handleChange }: ISwitch) => {
     <>
       <SEO title="Profile" description="Profile Modal of Nexus Application" />
 
-      <div className="rounded-3xl flex flex-col align-middle bg-boxcolor px-20 py-12">
+      <div className="rounded-3xl flex flex-col align-middle bg-boxcolor px-8 py-12 sm:px-20">
         <div className="flex justify-end mb-10 text-primarycolor">
-          <FaWindowClose />
+          <FaWindowClose className="w-5 h-5" />
         </div>
         <div className="flex flex-col items-center gap-2">
           <div className={styles.profileImage}>
@@ -33,11 +33,17 @@ const Profile = ({ checked, handleChange }: ISwitch) => {
 
           <h2 className="text-title1 font-bold text-text">Galo de Galochas</h2>
           <div className="flex mt-10 text-text font-bold text-title2 gap-8">
-            <div className="border-b-4">
-              <p className="flex">Librarys</p>
-            </div>
-            <div className="border-b-4 border-text">
-              <p>Edit profile</p>
+            <div className="flex">
+              <div className=" h-[100%] flex">
+                <button className="px-4 border-primarycolor text-primarycolor border-b-4 text-left w-[100%]">
+                  Library&apos;s
+                </button>
+              </div>
+              <div className=" h-[100%] flex">
+                <button className="border-text border-b-4 text-right px-4">
+                  Edit profile
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -50,26 +56,20 @@ const Profile = ({ checked, handleChange }: ISwitch) => {
           </div>
           <div className={styles.divUser}>
             <SiEpicgames className={styles.iconGame} />
-            <h2 className={styles.gameName}>
-              Epic Games
-            </h2>
+            <h2 className={styles.gameName}>Epic Games</h2>
             <p className={styles.usernameProfile}>Luwny</p>
             <AiOutlineEdit className={styles.editIcon} />
           </div>
           <div className={styles.divUser}>
             <SiPlaystation className={styles.iconGame} />
-            <h2 className={styles.gameName}>
-              Playstation
-            </h2>
+            <h2 className={styles.gameName}>Playstation</h2>
             <p className={styles.usernameProfile}>Ben Gadura</p>
             <AiOutlineEdit className={styles.editIcon} />
           </div>
 
           <div className="flex items-center relative">
             <SiXbox className={styles.iconGame} />
-            <h2 className={styles.gameName}>
-              Xbox G.P.
-            </h2>
+            <h2 className={styles.gameName}>Xbox G.P.</h2>
             <div className="flex items-center absolute right-[-17px]">
               <p className="text-headline3 font-medium text-text whitespace-nowrap overflow-hidden text-ellipsis w-[82px] text-left">
                 Ben Gadura
