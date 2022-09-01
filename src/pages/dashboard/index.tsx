01b8abfda433cfd2bef8695fa278fc0b00ef347c
Adam N <adaamangelow@gmail.com>
2022-08-31 21:32:07 -0300

    wip: dashboard recomended component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,12 @@
 import Background from "../../components/Background";
+import RecomendedDashboard from "../../components/RecomendedDashboard";
 import Header from './../../components/Header';
 
 export default function Dashboard() {
     return(
         <Background config="flex-col">
             <Header/>
+            <RecomendedDashboard />
         </Background>
     )
 }
