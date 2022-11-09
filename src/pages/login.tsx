b0271a5054a932e10ed3ee7bcaa40f20f30574b8
Marchi8 <luandeoliveirasoares@gmail.com>
2022-11-09 19:17:47 -0300

    refactor: login refactored

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,25 @@
 import { deleteCookie, getCookie } from "cookies-next";
 import { NextApiRequest, NextApiResponse } from "next";
 import React from "react";
+import BackgroundDashboard from "../components/BackgroundDashboard";
 import Background from "../components/BackgroundInitial";
+import GeralContainer from "../components/GeralContainer";
 import Login from "../components/Login";
+import SEO from "../components/SEO";
 import api from "../services/api";
 
 const login = () => {
   return (
     <>
-      <Background config="items-center justify-center ">
-        <Login />
-      </Background>
+      <BackgroundDashboard config="flex flex-col">
+        <SEO
+          title="Dashboard"
+          description="The NEXUS App simplifies your access to your games, unifying all platforms into one."
+        />
+        <div className="geral-content bg-[#0F1324] z-10 w-[90vw] h-[85vh] flex items-center justify-center rounded-[50px]">
+          <Login />
+        </div>
+      </BackgroundDashboard>
     </>
   );
 };
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,14 +1,24 @@
 import { deleteCookie, getCookie } from "cookies-next";
 import { NextApiRequest, NextApiResponse } from "next";
 import React from "react";
+import BackgroundDashboard from "../components/BackgroundDashboard";
 import Background from "../components/BackgroundInitial";
 import Register from "../components/Register";
+import SEO from "../components/SEO";
 import api from "../services/api";
 const register = () => {
   return (
-    <Background config="justify-center items-center">
-      <Register />
-    </Background>
+    <>
+      <BackgroundDashboard config="flex flex-col">
+        <SEO
+          title="Dashboard"
+          description="The NEXUS App simplifies your access to your games, unifying all platforms into one."
+        />
+        <div className="geral-content bg-[#0F1324] z-10 w-[90vw] h-[85vh] flex items-center justify-center rounded-[50px]">
+          <Register />
+        </div>
+      </BackgroundDashboard>
+    </>
   );
 };
 
