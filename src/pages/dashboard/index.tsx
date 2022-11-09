0c9b170bafc8fbb22e11e3d60963426a80f4f98e
Adam N <adaamangelow@gmail.com>
2022-11-09 15:13:08 -0300

    wip: new dashboard preparation

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,10 +3,15 @@ import BackgroundDashboard from "../../components/BackgroundDashboard";
 import GeralContainer from "../../components/GeralContainer";
 import LeftAside from "../../components/LeftAside";
 import RightSide from "../../components/RightSide";
+import SEO from "../../components/SEO";
 
 const Dashboard = () => {
   return (
     <BackgroundDashboard config="flex flex-col">
+      <SEO
+        title="Dashboard"
+        description="The NEXUS App simplifies your access to your games, unifying all platforms into one."
+      />
       <GeralContainer>
         <LeftAside />
 
